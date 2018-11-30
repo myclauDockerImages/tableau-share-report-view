4a6aa067762cee785f8b6d1e4610c11146c53004
myclau <myclau@gmail.com>
2018-11-30 17:16:04 +0800

    first commit

---
+++ 1.0.0/docker-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+if [ "${TABLEAU_HOST}" == "$null" ] || [ "${USERNAME}" == "$null" ]; then
+    echo "TABLEAU_HOST or USERNAME cannot be empty"
+    exit 1
+fi
+
+sed -i 's#${USERNAME}#'${USERNAME}'#g' /usr/src/app/server.js
+sed -i 's#${TABLEAU_HOST}#'${TABLEAU_HOST}'#g' /usr/src/app/server.js
+sed -i 's#${PORT}#'${PORT-8080}'#g' /usr/src/app/server.js
+sed -i 's#${IFRAME_WIDTH}#'${IFRAME_WIDTH-1000}'#g' /usr/src/app/server.js
+sed -i 's#${IFRAME_HEIGHT}#'${IFRAME_HEIGHT-600}'#g' /usr/src/app/server.js
+exec "$@"
